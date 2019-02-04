57a01f3c2cef2a1545dc8843af62b366ea5084f3
pranavdaa <pranavmaheshwari4321@gmail.com>
2019-02-05 00:00:33 +0530

    new commit just for trsting the about page

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ABOUT</h1><br/><br/>
+        <h2>This is just a hackathon based project for Web-dev challenge of IIITBBSR </h2><br/>
+        <h2>We have created a fake auction Platform For selling and buying stuff </h2><br/>
+        <h2>Tech stack used is JAVASCRIPT NODEJS REACT </h2><br/>
+        <h2>First the user has to sigup Then he can go in the dashboard and place his bid </h2><br/>
+        <h2> IF the bid is sucessfull the user is able to buy the thing</h2>
+      </div>
+    );
+  }
+}
