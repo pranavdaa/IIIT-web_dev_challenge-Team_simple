b355f4889866cbb31bd93b91996c16c484507134
pranavdaa <pranavmaheshwari4321@gmail.com>
2019-02-05 09:07:31 +0530

    Importantnt commit some more pages added

---
+++ src/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Schedule = () => (
+  <div>
+    <ul>
+      <li>Pranav Maheshwari</li>
+      <li>Mobile Number</li>
+      <li>8917276615</li>
+    </ul>
+  </div>
+)
+
+export default Schedule
